4c0ed575d548c83f631c94c646b3e333e42b6fdf
rmnull <slackErEhth77@openmailbox.org>
2019-05-17 14:44:17 +0530

    ughhhhh

--- client-rough.rkt
+++ client-rough.rkt
@@ -3,6 +3,7 @@
 (require json)
 
 (define *PORT* 8428)
+(define PEERNAME "λ")
 
 (define (join peer link)
   (define req (format "JOIN\n~a\n~a" peer link))
@@ -19,11 +20,12 @@
   (displayln chunk (current-error-port))
   (define range (string-join (map ~a chunk) "-"))
   (define fname (string-append range ".part"))
-  (define status
-    (system "curl -r ~a -o ~a ~a" range fname link))
+  (define cmd
+    (format "curl -r ~a -o ~a ~a 2>/dev/null 1>&2" range fname link))
+  (define status (system cmd))
   status)
 
-(define (ask-req-cycle peer token)
+(define (ask-req-cycle peer token #:callback [cb void])
   (define (handle-done c)
     (not (hash-ref c 'completed)))
 
@@ -61,9 +63,81 @@
         (error (hash-ref c 'message "flop")))
     ))
 
-(define token
+
+
+
+(define resp
   (join "JALLU"
         "https://www.nasa.gov/sites/default/files/thumbnails/image/stsci-h-p1930a-f-3213x2836.jpg"))
+
+(when (equal? (hash-ref resp 'status) "OK")
+  (define token (hash-ref resp 'token))
+  (ask-req-cycle PEERNAME token))
+
+
+(require racket/gui)
+(define main-window
+  (new frame% [label "Client sketch"]
+       [width 800] [height 600]
+       [stretchable-width #t] [stretchable-height #t]))
+
+(define (clear-main-window)
+  (send main-window change-children (λ (c) '())))
+
+(define (verify-link-and-dl link)
+  void )
+
+(define (draw-join)
+  (clear-main-window)
+  (define row (new horizontal-panel%
+                   [stretchable-height #f]
+                   [parent main-window]))
+  (define link-field
+    (new text-field% (label "Link") (parent row) (init-value "")
+         (style '(single vertical-label))))
+
+  (define log-field (new message% (label "") (parent main-window)))
+
+  (define go-btn (new button% (parent row) (label "Get Token")
+                      (callback (λ (_m _b)
+                                  (define link (send link-field get-value))
+                                  (define msg (verify-link-and-dl link))
+                                  (send log-field set-label msg)))))
+  (send link-field get-value))
+
+(define (update-progress)
+  (void))
+
+
+(define (draw-dl)
+  (clear-main-window)
+  (define row (new horizontal-panel%
+                   [stretchable-height #f]
+                   [parent main-window])) 
+  (define token-field
+    (new text-field% (label "token") (parent row) (init-value "")
+         (style '(single vertical-label))))
+  (define log-field (new message% (label "") (parent main-window)))
+  (define go-btn (new button% (parent row) (label "Start Download")
+                      (callback (λ (_m _b)
+                                  (define token (send token-field get-value))
+                                  (define msg
+                                    (ask-req-cycle PEERNAME token #:callback update-progress))
+                                  (send log-field set-label msg)))))
+  (void))
+
+(define menu-bar (new menu-bar% [parent main-window]))
+(define m (new menu% [label "Options"] [parent menu-bar]))
+(new menu-item% [parent m] [label "Join"] [callback (λ (m c) (draw-join))])
+(new separator-menu-item% [parent m])
+(new menu-item% [parent m] [label "Download"]
+     [callback (λ (m c) (draw-dl))])
+(new separator-menu-item% [parent m])
+(new menu-item% [parent m] [label "Exit"]
+     [callback (λ (m c) (send main-window show #f))])
+
+
+(send main-window show true)
 
 
 #|
